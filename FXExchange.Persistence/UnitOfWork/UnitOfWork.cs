7ec90110e6b14514e8275e7200f9a64b4106eb70
shyam <shyam@DESKTOP-LLNJELL>
2026-03-24 08:53:45 +0100

    ..

--- FXExchange.Persistence/UnitOfWork/UnitOfWork.cs
+++ FXExchange.Persistence/UnitOfWork/UnitOfWork.cs
@@ -8,12 +8,9 @@ public sealed class UnitOfWork : IUnitOfWork
 {
     private readonly FxDbContext _context;
 
-    private readonly ILogger<UnitOfWork>
-        _logger;
+    private readonly ILogger<UnitOfWork> _logger;
 
-    public UnitOfWork(
-        FxDbContext context,
-        ILogger<UnitOfWork> logger)
+    public UnitOfWork( FxDbContext context, ILogger<UnitOfWork> logger)
     {
         ArgumentNullException.ThrowIfNull(context);
 
@@ -26,23 +23,23 @@ public sealed class UnitOfWork : IUnitOfWork
 
     public async Task Save( )
     {
+        using var transaction = _context.Database.BeginTransaction();
+        
         try
         {
-            _logger.LogInformation(
-            "Saving database changes");
+            _logger.LogInformation("Saving database changes");
 
-            await _context
-                .SaveChangesAsync();
+            await _context.SaveChangesAsync();
+            transaction.Commit();
 
-            _logger.LogInformation(
-            "Database saved");
+            _logger.LogInformation("Database saved");
+            
         }
         catch (Exception ex)
         {
-            _logger.LogError(
-            ex,
-            "Database save failed");
-
+            _logger.LogError( ex, "Database save failed");
+            transaction.Rollback();
+            _logger.LogError(ex, "Database save Rollbacked");
             throw;
         }
     }
